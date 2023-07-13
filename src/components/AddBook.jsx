8aa3752188d7fb0bc874e79392c48a854cbe6267
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-07-13 18:54:55 +0300

    Cerated the AddBook componenet with it's state

---
+++ src/components/AddBook.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const AddBook = ({ addBook }) => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addBook({ title, author });
+    setTitle('');
+    setAuthor('');
+  };
+
+  return (
+    <div>
+      <h2>Add Book</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <input
+          type="text"
+          id="author"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
+};
+
+AddBook.propTypes = {
+  addBook: PropTypes.func.isRequired,
+};
+
+export default AddBook;
