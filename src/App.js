c5a9672faee004d9383aed2f0c8a96d360515cf5
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-07-13 19:09:11 +0300

    Cerate the category page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Book from './pages/Book';
+import Categories from './pages/Categories';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Book />} />
+        <Route path="/categories" element={<Categories />} />
       </Routes>
     </>
   );
---
+++ src/pages/Categories.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Categories = () => (
+  <h2>This page is currently being developed and will be available soon.</h2>
+);
+
+export default Categories;
