b7e89eb1df69bb6aa412f959e3a0aa0952e0b0ea
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-07-18 16:32:21 +0300

    Added the book intial state

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,11 +1,30 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  books: [],
+  books: [
+    {
+      item_id: "item1",
+      title: "The Great Gatsby",
+      author: "John Smith",
+      category: "Fiction",
+    },
+    {
+      item_id: "item2",
+      title: "Anna Karenina",
+      author: "Leo Tolstoy",
+      category: "Fiction",
+    },
+    {
+      item_id: "item3",
+      title: "The Selfish Gene",
+      author: "Richard Dawkins",
+      category: "Nonfiction",
+    },
+  ],
 };
 
 export const booksSlice = createSlice({
-  name: 'books',
+  name: "books",
   initialState,
   reducers: {
     addBook: (state, action) => {
